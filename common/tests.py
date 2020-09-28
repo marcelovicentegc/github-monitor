013a1ee3faf411f3e46c352626528c712669dbfb
Marcelo Cardoso <marcelovicentegc@gmail.com>
2020-09-28 10:21:06 -0300

    test: authenticated index view

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.test import Client, RequestFactory, TestCase
 
 
@@ -32,3 +34,21 @@ class Tests(TestCase):
             status_code=302,
             target_status_code=200,
             fetch_redirect_response=True)
+
+    def test_authenticated_index_view(self):
+        user = User.objects.create_user(
+            'john',
+            'lennon@thebeatles.com',
+            'johnpassword',
+            is_active=True,
+            is_staff=True,
+            is_superuser=True)
+        user.save()
+        user = authenticate(username='john', password='johnpassword')
+        client = Client()
+        response = client.get('/login')
+        login = client.login(username='john', password='johnpassword')
+        self.assertTrue(login)
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'common/index.html')
